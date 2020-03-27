3a1b779cc378c8a83a90e9f0b69d0a2fb74bab9b
EthanJM9 <55208847+EthanJM9@users.noreply.github.com>
2020-03-27 16:23:58 -0400

    Uploaded Files

---
+++ SetIC.m
@@ -0,0 +1,16 @@
+IC.P = 0;
+IC.Q = 0;
+IC.R  = 0;
+IC.Phi = 0;
+IC.The = 0;
+IC.Psi = 0;
+IC.U = 0;
+IC.V = 0;
+IC.W = 0;
+IC.X = 5;
+IC.Y = 5;
+IC.Z = 5;
+IC.w1 = 200;
+IC.w2 = 200;
+IC.w3 = 200;
+IC.w4 = 200;
